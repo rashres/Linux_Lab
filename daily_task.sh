3e776076fa3c584cca6c93a470feca7202c31198
username <user@email.com>
2025-10-21 23:23:19 -0500

    Added script execution flow in daily_task.sh

---
+++ daily_task.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Part (a): Run lab_tools.sh and system_report.sh sequentially
+
+echo "Running daily automation tasks..."
+echo "----------------------------------"
+
+./lab_tools.sh
+./system_report.sh
