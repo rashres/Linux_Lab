c972e86d7b790db19defa9c3db1d1e86ba199c69
username <user@email.com>
2025-10-21 23:29:03 -0500

    Added use_modules.sh to source and call lab functions

---
+++ use_modules.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Part (b): Source the function library and run all tools
+
+source ./lab_modules.sh
+
+echo "---- Running all lab module functions ----"
+show_date
+show_user
+show_uptime
+show_disk
+echo "------------------------------------------"
